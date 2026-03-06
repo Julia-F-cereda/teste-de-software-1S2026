6f24826278d909190ccc82a2d5fee6b1503490ea
julia cereda <julia.cereda@aluno.senai.br>
2026-03-06 17:17:51 -0300

    ADD: teste funcionando e programa totalmente correto aparentemente

--- escola.py
+++ escola.py
@@ -1,13 +1,28 @@
 def v_media(media: int | float) -> str:
-    """essa função retorna se o aluno passou ou nao de ano                         """
-    if media >= 7:
-        return("aprovado")
-    elif media <5:
-        return("reprovado")
-    elif media >= 5 and media < 7:
-        return("recuperação")          
-    elif media <0 and media > 10:
-        return("digite um numero entre 0 a 10")
+    """essa função retorna se o aluno passou ou nao de ano"""
+    #verificando se é uma string. O raise faz ele dar erro direto
+    if not isinstance(media,(int,float)):
+        raise TypeError("Tipo inválido, a entrada deve ser numerica")
     
+    #verificando se o numero é menor que 0, se for ele vai printar um erro de valor
+    if media <0:
+        raise ValueError("Valor invalido, entrada tem que ser maior que 0")
+    
+    #verificando se o numero é maior que 10, se for printa erro de valor
+    if media >10:
+        raise ValueError("Valor invalido, entrada tem que ser menor que 10")
+    
+    #verificando se maior que 7 aprovado
+    if media >= 7:
+        return "aprovado"
+    
+    #verificando se menor que 4 é reprovado
+    elif media <=4:
+        return "reprovado"
+    #se nao estiver nos criterios acima vai pra recuperação
+    else:
+        return "recuperação"      
+    
+   
 if __name__ == "__main__":
     print(v_media(7))
--- test/test_v_media_erros.py
+++ test/test_v_media_erros.py
@@ -2,5 +2,14 @@ import pytest
 from escola import v_media
 
 def test_string():
-    with pytest.raises(TypeError, match="Tipo invalido, entrada tem que ser numérica"):
+    with pytest.raises(TypeError, match="Tipo inválido, a entrada deve ser numerica"):
         v_media("oito")
+
+def test_value_menor():
+    with pytest.raises(ValueError, match="Valor invalido, entrada tem que ser maior que 0"):
+        v_media(-5)
+
+def test_value_maior():
+    with pytest.raises(ValueError, match="Valor invalido, entrada tem que ser menor que 10"):
+        v_media(2000)
+
